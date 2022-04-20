3f65287ac0762229582eb59dc2cd9212b1c77338
David Vincent <70216205+jazzyboii@users.noreply.github.com>
2022-04-20 16:41:08 -0400

    interactive button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={function() {console.log('click');  }}>
+          {this.props.value}
         </button>
       );
     }
@@ -14,7 +15,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
@@ -65,5 +66,4 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
-  
   
